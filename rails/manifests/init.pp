f49fff987a63ccb176e2e93b7e6ebe2cc936a2e4
jokerswild663 <jokerswild663@gmail.com>
2013-06-10 02:43:59 +0000

    refactored rails installation to external class

--- rails/manifests/init.pp
+++ rails/manifests/init.pp
@@ -3,52 +3,8 @@ class rails {
 	class {'rails::pr':
 	}
 
-	package{'ruby1.9.3':
-		ensure		=> present,
+	class {'rails::install':
 	}
 
-	#package{'build-essential':
-	#	ensure		=> present
-	#}
-
-	package{'rails':
-		ensure		=> present,
-		provider	=> gem
-	}
-
-	package{'libsqlite3-dev':
-		ensure		=> present,
-	}
-
-	package{'sqlite3':
-		ensure 		=> present,
-		provider	=> gem
-	}
-
-	package{'nodejs':
-		ensure		=> present
-	}
-
-	package{'mysql-server':
-		ensure 		=> present,
-	}
-
-	package{'mysql-client':
-		ensure		=> present
-	}
-
-	package{'libmysql-ruby':
-		ensure		=> present
-	}
-
-	package{'libmysqlclient-dev':
-		ensure		=> present
-	}
-
-	package{'mysql':
-		ensure		=> present,
-		provider	=> gem
-	}
-
-	Class['rails::pr'] -> Package['ruby1.9.3'] -> Package['rails'] -> Package['libsqlite3-dev'] -> Package['sqlite3'] -> Package['nodejs'] -> Package['mysql-server'] -> Package['mysql-client'] -> Package['libmysql-ruby'] -> Package['libmysqlclient-dev'] -> Package['mysql']
+	Class['rails::pr'] -> Class['rails::install']
 }
