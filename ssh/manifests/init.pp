467a1bd60ac07dec34e689fb6fa099924f253767
jokerswild663 <jokerswild663@gmail.com>
2013-09-06 03:59:39 +0000

    Added ssh module

--- ssh/manifests/init.pp
+++ ssh/manifests/init.pp
@@ -1,3 +1,36 @@
 class ssh {
-	notify {"working":}
+  include ssh::install, ssh::config, ssh::service
+}
+
+class ssh::install {
+  package { "openssh-server":
+    ensure => installed,
+  }
+
+  package { "openssh-client":
+    ensure => installed,
+    require=> Package["openssh-server"]
+  }
+}
+
+class ssh::config {
+  file { "/etc/ssh/sshd_config":
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => 0600,
+    source => "puppet:///modules/ssh/sshd_config",
+    require=> Class["ssh::install"],
+    notify => Class["ssh::service"],
+  }
+}
+
+class ssh::service {
+  service { "sshd":
+    ensure    => running,
+    hasstatus => true,
+    hasrestart=> true,
+    enable    => true,
+    require   => Class["ssh::config"],
+  }
 }
